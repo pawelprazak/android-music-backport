483bd11ae26366984962017ca97850f5c2fad159
Paweł Prażak <pawelprazak@gmail.com>
2011-12-02 00:50:02 +0100

    Simplify: clarify the code and use System.arraycopy()

--- src/com/android/music/MediaPlaybackService.java
+++ src/com/android/music/MediaPlaybackService.java
@@ -784,16 +784,15 @@ public class MediaPlaybackService extends Service {
     }
 
     private void ensurePlayListCapacity(int size) {
-        if (mPlayList == null || size > mPlayList.length) {
+        if (mPlayList == null) {
+            mPlayList = new long[size * 2];  
+        } else if (size > mPlayList.length) {
             // reallocate at 2x requested size so we don't
             // need to grow and copy the array for every
             // insert
-            long [] newlist = new long[size * 2];
-            int len = mPlayList != null ? mPlayList.length : mPlayListLen;
-            for (int i = 0; i < len; i++) {
-                newlist[i] = mPlayList[i];
-            }
-            mPlayList = newlist;
+            long [] newList = new long[size * 2];
+            System.arraycopy(mPlayList, 0, newList, 0, mPlayList.length);
+            mPlayList = newList;
         }
         // FIXME: shrink the array when the needed size is much smaller
         // than the allocated size
